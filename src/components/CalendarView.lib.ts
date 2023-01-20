ce922a5049d42e0116aa97afdcbd76252700ec55
Damian Thomas <dimitrisonic@gmail.com>
2023-01-19 20:19:21 -0500

    cell state updates working

--- src/components/CalendarView.lib.ts
+++ src/components/CalendarView.lib.ts
@@ -1,8 +1,31 @@
-import { Weekday, weekdays } from "../types";
+import { DOMAttributes } from "react";
+import { TimeBlock, Weekday, weekdays } from "../types";
+
+export type CalendarState = {
+	divisions: boolean[],
+	setDivisions: React.Dispatch<React.SetStateAction<boolean[]>>
+}
 
 export function sortWeek(startDay: Weekday) {
 	const startIndex = weekdays.indexOf(startDay);
 	return weekdays
 		.slice(startIndex)
 		.concat(weekdays.slice(0, startIndex));
+}
+
+export function bindCell({ setDivisions }: CalendarState, position: number): DOMAttributes<HTMLDivElement> {
+	// loops through divisions, finds the value at divisions[position], switches it, then returns a new array
+	const updateCell = () => setDivisions(divisions => {
+		console.log("updating subcell at position " + position);
+		return divisions.map(
+			(subcell, index) => index == position ? !subcell : subcell
+		);
+	}
+	);
+
+
+
+	return {
+		onClick: updateCell
+	};
 }
--- src/components/CalendarView.tsx
+++ src/components/CalendarView.tsx
@@ -1,44 +1,65 @@
-import { Weekday } from "../types";
-import { sortWeek } from "./CalendarView.lib";
+import { createContext, useContext, useState } from "react";
+import { TimeBlock, Weekday, weekdays } from "../types";
+import { bindCell, CalendarState, sortWeek } from "./CalendarView.lib";
 import styles from "./CalendarView.module.scss";
 
+const CalendarContext = createContext<CalendarState | null>(null);
 export function CalendarView(props: {
 	startDay: Weekday,
 	startDate: number, // zero-indexed
 	hours: number // zero-indexed
 }) {
+	const [divisions, setDivisions] = useState<boolean[]>(
+		new Array(7 * props.hours * 2).fill(false)
+	);
 
 	const week = sortWeek(props.startDay);
 	console.log(week);
 
 	return (
-		<div className={styles.Container}>
-			{week.map((weekday, index) =>
-				<DayColumn key={index} weekday={weekday} date={props.startDate + index} hours={props.hours} />
-			)}
-		</div>
+		<CalendarContext.Provider value={{ divisions, setDivisions }}>
+			<div className={styles.Container}>
+				{week.map((weekday, index) =>
+					<DayColumn
+						key={index}
+						weekIndex={index}
+						weekday={weekday}
+						date={props.startDate + index}
+						hours={props.hours}
+					/>
+				)}
+			</div>
+		</CalendarContext.Provider>
 	);
 }
 
-
 function DayColumn(props: {
 	weekday: Weekday,
+	weekIndex: number,
 	date: number, // zero-indexed
 	hours: number // zero-indexed
 }) {
-
-	const divisions = new Array(props.hours)
+	const state = useContext(CalendarContext);
+	if (!state) { return null; }
+	const subcells = new Array(props.hours)
 		.fill(null)
-		.map((_, index) => <div key={index} className={styles.Cell}>
-			<div className={styles.Subcell}></div>
-			<div className={styles.Subcell}></div>
-		</div>);
+		.map((_, hour) => {
+			const cellLocation = (props.weekIndex * props.hours * 2) + (hour * 2);
+			return <div key={hour} className={styles.Cell}>
+				<div {...bindCell(state, cellLocation)} className={
+					`${styles.Subcell} ${state.divisions[cellLocation] ? styles.Active : ""}`
+				}>{cellLocation}</div>
+				<div {...bindCell(state, cellLocation + 1)} className={
+					`${styles.Subcell} ${state.divisions[cellLocation + 1] ? styles.Active : ""}`
+				}>{cellLocation + 1}</div>
+			</div>;
+		});
 
 	return (
 		<div className={styles.Column}>
-			<h3>{props.weekday.slice(0, 3)}</h3>
+			<h3>{props.weekday.slice(0, 3)} {props.weekIndex}</h3>
 			<h4>{props.date + 1}</h4>
-			{divisions}
+			{subcells}
 		</div>
 	);
 }
